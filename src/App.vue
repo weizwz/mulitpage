8a938acc4cf109f5c63fd99b0d8907802f64891e
wyc <1124725517@qq.com>
2022-11-19 00:51:45 +0800

    增加多界面配置

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <p><a href="/about">go to about</a></p>
   </div>
 </template>
 
